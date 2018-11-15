a74e6a48f4b3ec390db710abf207791421756776
Arsen <arsencho523@gmail.com>
2018-11-15 17:29:11 +0400

    kap chuni

--- script.js
+++ script.js
@@ -58,6 +58,7 @@ var matrix = matrixo(40, 40);
 var n = 10;
 var m = n;
 var side = 10;
+var days = 0
 var grassArr = [];
 var grassEaterArr = [];
 var wildArr = [];
@@ -70,7 +71,9 @@ var doubleArr = [];
 //   		        [4,4,4,4,4],
 //   		        [4,4,4,4,5]
 //   		   ];
-
+//var weathers = ["autumn","spring","summer","winter"] 
+var weather = "winter";
+    
 function setup() {
     frameRate(2);
     background('#acacac');
@@ -98,33 +101,78 @@ function setup() {
     createCanvas(matrix.length * side, matrix.length * side);
 }
 function draw() {
+    days++;
+     if(days<50){
+         t="winter"; 
+         document.getElementById("text").innerHTML = "Weather - Winter";
+     }
+     else if(days>=50 && days<150){
+         t="spring"; 
+         document.getElementById("text").innerHTML = "Weather - Spring";
+     }
+     else if(days>=150 && days<250){
+         t="summer"; 
+         document.getElementById("text").innerHTML = "Weather - Summer";
+     }
+     else if(days>=250 && days<500){
+         t="autumn"; 
+         document.getElementById("text").innerHTML = "Weather - Autumn";
+     }
+    // if(days<10){
+    //     t="winter"; //winter
+    // }
+    // else if(days>=10 && days<20){
+    //     t="spring"; //spring
+    // }
+    // else if(days>=20 && days<30){
+    //     t="summer"; //spring
+    // }
+    // else if(days>=30 && days<40){
+    //     t="autumn"; //spring
+    // }
+    else{
+        days = 0; 
+    }
+
     for (var y = 0; y < matrix.length; y++) {
         for (var x = 0; x < matrix[y].length; x++) {
 
             if (matrix[y][x] == 1) {
-                fill("green");
-                rect(x * side, y * side, side, side);
+                if(t=="winter"){
+                    fill("white");
+                }
+                else if(t=="autumn"){
+                    fill("blue");
+                }
+                else{
+                    fill("green");
+                }
+
+                
+                //rect(x * side, y * side, side, side);
             }
             else if (matrix[y][x] == 0) {
                 fill("#acacac");
-                rect(x * side, y * side, side, side);
+                //rect(x * side, y * side, side, side);
             }
             else if (matrix[y][x] == 2) {
                 fill("yellow");
-                rect(x * side, y * side, side, side);
+                //rect(x * side, y * side, side, side);
             }
             else if (matrix[y][x] == 3) {
                 fill("red");
-                rect(x * side, y * side, side, side);
+                //rect(x * side, y * side, side, side);
             }
             else if (matrix[y][x] == 4) {
                 fill("orange");
-                rect(x * side, y * side, side, side);
+               //rect(x * side, y * side, side, side);
             }
             else if (matrix[y][x] == 5) {
                 fill("#9933ff");
-                rect(x * side, y * side, side, side);
+                
             }
+            
+            rect(x * side, y * side, side, side);
         }
     }
     for (var i in grassArr) {
@@ -163,4 +211,3 @@ function matrixo(m) {
     }
     return matrix;
 }
-
